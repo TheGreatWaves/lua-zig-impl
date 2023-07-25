2bebeca09b5ed081889d99cf90f1eb77b371e357
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-07-26 00:52:31 +0700

    feat: added tests

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,14 +1,23 @@
+pub const std = @import("std");
 pub const CpuError = error{Unknown};
+pub const Bus = @import("bus.zig");
 
 const PC_START = 0;
 
 // 6502 emulation.
 pub const Cpu = struct {
     PC: u32,
+    bus: ?*Bus.Bus,
 
     pub fn make() Cpu {
-        return Cpu{
-            .PC = PC_START,
-        };
+        return Cpu{ .PC = PC_START, .bus = null };
+    }
+
+    pub fn connectBus(this: *Cpu, bus: ?*Bus.Bus) CpuError!void {
+        if (bus) |b| {
+            this.bus = b;
+        } else {
+            return CpuError.Unknown;
+        }
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const bus = @import("bus.zig");
 
 pub fn main() !void {
-    var system = bus.Bus.make();
+    var system = bus.Bus.make() catch unreachable;
     system.ram.dump(5, 10);
     system.write(0x00FF, 0xfe);
     system.ram.dump(0x00F0, 0x0100);
